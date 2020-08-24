9ab8404dd4cd86645e92b4d096918edfd5dc5cc9
Emmanuel <emmanuelallison@outlook.com>
2020-08-24 17:05:39 +0100

    Created QuestionWhatever (sorry, I ran out of names)

---
+++ Fingerprint Authentication/QuestionWhatever.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Fingerprint_Authentication
+{
+    /// <summary>
+    /// Interaction logic for QuestionWhatever.xaml
+    /// </summary>
+    public partial class QuestionWhatever : Window
+    {
+        MainWindow mainWindow;
+
+        public QuestionWhatever()
+        {
+            InitializeComponent();
+        }
+
+        private void enrollButton_Click(object sender, RoutedEventArgs e)
+        {
+            startMainWindow("enroll");
+        }
+
+        private void verifyButton_Click(object sender, RoutedEventArgs e)
+        {
+            startMainWindow("verify");
+        }
+
+        private void startMainWindow(string functionToExecute)
+        {
+            mainWindow = new MainWindow(functionToExecute);
+            Close();
+            mainWindow.Show();
+        }
+    }
+}
