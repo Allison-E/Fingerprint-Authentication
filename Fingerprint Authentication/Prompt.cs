50388cd9b4b57ee5e42524b36ff4a96299ecafff
Emmanuel <emmanuelallison@outlook.com>
2020-08-24 13:54:11 +0100

    Added prompt

---
+++ Fingerprint Authentication/Prompt.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fingerprint_Authentication
+{
+    public class Prompt: INotifyPropertyChanged
+    {
+        private string picture;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Picture
+        {
+            get
+            {
+                return picture;
+            }
+            set
+            {
+                if (picture != value)
+                {
+                    picture = value;
+                    onPropertyChanged();
+                }
+            }
+        }
+
+        private void onPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
