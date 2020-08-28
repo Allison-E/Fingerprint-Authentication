7c1ffe0d9d0ed9b9f2d11e0d3f205b5bf1607a8a
Emmanuel <emmanuelallison@outlook.com>
2020-08-28 02:45:49 +0100

    Adjusted the code here in response to the changes made to DBHandler.cs

--- Fingerprint Authentication/MainWindow.Enrollment-related.cs
+++ Fingerprint Authentication/MainWindow.Enrollment-related.cs
@@ -9,78 +9,89 @@ using DPFP;
 namespace Fingerprint_Authentication
 {
 	// Fingerprint enrollment methods are here
-    partial class MainWindow
-    {
-        private Enrollment enroller;
+	partial class MainWindow
+	{
+		private Enrollment enroller;
 
-        private void initialiseEnroller()
-        {
-            try
-            {
-                enroller = new Enrollment();
-                WriteGoodStatus("Enroller initialised.");
-            }
-            catch
-            {
-                WriteErrorStatus("Could not initialise enroller.");
-            }
-        }
+		private void initialiseEnroller()
+		{
+			try
+			{
+				enroller = new Enrollment();
+				WriteGoodStatus("Enroller initialised.");
+			}
+			catch
+			{
+				WriteErrorStatus("Could not initialise enroller.");
+			}
+		}
 
-        private void startEnrolling()
-        {
-            initialiseEnroller();
-            noOfScansLeft = enroller.FeaturesNeeded;
-            WriteStatus("Put your finger on the scanner.");
-            WriteStatus($"Scans left: {noOfScansLeft}.");
-        }
+		private void startEnrolling()
+		{
+			initialiseEnroller();
+			noOfScansLeft = enroller.FeaturesNeeded;
+			WriteStatus("Put your finger on the scanner.");
+			WriteStatus($"Scans left: {noOfScansLeft}.");
+		}
 
-        private void createFeatureAndAddItToTheEnroller(Sample sample)
-        {
-            FeatureSet feature = ExtractFeatures(sample, DataPurpose.Enrollment);
+		private void createFeatureAndAddItToTheEnroller(Sample sample)
+		{
+			FeatureSet feature = ExtractFeatures(sample, DataPurpose.Enrollment);
 
-            if (feature != null)
-            {
-                WriteGoodStatus("The fingerprint feature set was created.");
-                enroller.AddFeatures(feature);
-                noOfScansLeft--;
-            }
+			if (feature != null)
+			{
+				WriteGoodStatus("The fingerprint feature set was created.");
+				enroller.AddFeatures(feature);
+				noOfScansLeft--;
+			}
 
-            if (noOfScansLeft != 0)
-                WriteStatus($"Scans left: {noOfScansLeft}.");
-        }
+			if (noOfScansLeft != 0)
+				WriteStatus($"Scans left: {noOfScansLeft}.");
+		}
 
-        private void processEnrollment(Sample sample)
-        {
-            createFeatureAndAddItToTheEnroller(sample);
+		private async void processEnrollment(Sample sample)
+		{
+			createFeatureAndAddItToTheEnroller(sample);
 
-            switch (enroller.TemplateStatus)
-            {
-                case Enrollment.Status.Ready:   // Report success and stop capturing
-                    byte[] byteArray = null;
-                    StopCapturing();
-                    // Todo: Add call to save the enrolled fingerprint
-                    enroller.Template.Serialize(ref byteArray);
+			switch (enroller.TemplateStatus)
+			{
+				case Enrollment.Status.Ready:   // Report success and stop capturing
+					byte[] byteArray = null;
+					StopCapturing();
+					// Todo: Add call to save the enrolled fingerprint
+					enroller.Template.Serialize(ref byteArray);
 
-                    if (db.StoreFingerprintInDBAsync(enroller.Template))
-                    {
-                        System.Windows.MessageBox.Show("Fingerprint enrollment was successful!", "Success!", System.Windows.MessageBoxButton.OK);
-                        System.Windows.Application.Current.Shutdown();
-                    }
-                    else
-                    {
-                        System.Windows.MessageBox.Show("Fingerprint enrollment was unsuccessful!", "Sorry!", System.Windows.MessageBoxButton.OK);
-                        System.Windows.Application.Current.Shutdown();
-                    }
+					bool storageWasSuccessful = false;
+					try
+					{
+						var store = db.StoreFingerprintInDBAsync(byteArray);
+						storageWasSuccessful = await store;
+					}
+					catch (DB.CouldNotStoreFingerprintInDBException)
+					{
+						System.Windows.MessageBox.Show("Fingerprint enrollment was unsuccessful!", "Sorry!", System.Windows.MessageBoxButton.OK);
+						System.Windows.Application.Current.Shutdown();
+			}
 
-                    break;
+					if (storageWasSuccessful)
+					{
+						System.Windows.MessageBox.Show("Fingerprint enrollment was successful!", "Success!", System.Windows.MessageBoxButton.OK);
+						System.Windows.Application.Current.Shutdown();
+					}
+					else
+					{
+						System.Windows.MessageBox.Show("Fingerprint enrollment was unsuccessful!", "Sorry!", System.Windows.MessageBoxButton.OK);
+						System.Windows.Application.Current.Shutdown();
+					}
+					break;
 
-                case Enrollment.Status.Failed:  // Report failure and restart capturing
-                    enroller.Clear();
-                    StopCapturing();
-                    StartCapturing();
-                    break;
-                    
-            }
-        }
-    }
+				case Enrollment.Status.Failed:  // Report failure and restart capturing
+					enroller.Clear();
+					StopCapturing();
+					StartCapturing();
+					break;
+					
+			}
+		}
+	}
 }
